17211a4a6a5bb52d25b2667414203041cdebf874
jotaroya <jyoracewalking@gmail.com>
2025-06-27 22:57:39 +0900

    Kansei3_darknashi

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="">
+    <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
